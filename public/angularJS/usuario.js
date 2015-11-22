157746e6f6b435b1b2a28a3fda532627e336a9d8
roycer <roycercordova@gmail.com>
2015-11-22 16:45:54 -0500

    mis cambios login

--- public/angularJS/usuario.js
+++ public/angularJS/usuario.js
@@ -1,11 +1,12 @@
 var app = angular.module('myAppUsuario',[]);
 
-app.controller('myCtrlUsuario',  function($scope,$http) {
+app.controller('myCtrlUsuario',  function($scope,$http,$window) {
   $scope.cuenta = {username:'',password:''};
   $scope.ingresar = function(){
 	  
 	  $http.post('/fusuario/login', $scope.cuenta)
 		.success(function(data) {
+				$window.location.href = "/home";
 				$scope.cuenta = {}; // Borramos los datos del formulario
 				$scope.respuestaServer = data;
 				alert(JSON.stringify($scope.respuestaServer));
