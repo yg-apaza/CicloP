ce5a86c086369fe5549a7eedb5ecf985f0bc57d1
roycer <roycercordova@gmail.com>
2015-11-22 15:05:55 -0500

    agregando metodo post al login

--- public/angularJS/usuario.js
+++ public/angularJS/usuario.js
@@ -1,24 +1,29 @@
 var app = angular.module('myAppUsuario',[]);
 
-app.controller('myCtrlUsuario',  function($scope) {
+app.controller('myCtrlUsuario',  function($scope,$http) {
  $scope.cuenta = {usuario:'',clave:''};
 
   $scope.ingresar = function(){
-        alert(JSON.stringify($scope.cuenta));
+	  $scope.cuenta = {usuario:'', clave:''};
+	  
+	  $http.post('/fusuario/login', $scope.cuentaReg)
+		.success(function(data) {
+				$scope.cuenta = {}; // Borramos los datos del formulario
+				$scope.respuestaServer = data;
+				alert(JSON.stringify($scope.respuestaServer));
+		});
   };
 });
 
 app.controller('myCtrlUsuarioReg',  function($scope,$http) {
-	$scope.cuentaReg = {nombre:'', apellidos:'', usuario:'', correo:'', clave:'', claveVerificacion:''};
+	$scope.cuentaReg = {nombre:'', apellidos:'', usuario:'', correo:'', clave:''};
 	
 	$scope.registrar = function(){
-		
 		$http.post('/fusuario/register', $scope.cuentaReg)
 		.success(function(data) {
-				$scope.newPersona = {}; // Borramos los datos del formulario
+				$scope.cuentaReg = {}; // Borramos los datos del formulario
 				$scope.respuestaServer = data;
 				alert(JSON.stringify($scope.respuestaServer));
-				
 		});
-  };
+	};
 });
--- routes/fusuario.js
+++ routes/fusuario.js
@@ -14,8 +14,8 @@ router.post('/validate', function(req, res) {
 /** Registro */
 router.post('/register', function(req, res) {
 	Usuario.register(new Usuario({nombre: req.body.nombre, apellidos: req.body.apellidos, username: req.body.usuario, correo: req.body.correo}), req.body.clave, function(err, usuario) {
-		if((err.name).equals("UserExistsError"))
-			return res.redirect('/').json({status: false, message: "Ya existe ese nombre de usuario."});
+		if(err && (err.name == "UserExistsError"))
+			return res.json({status: false, message: "Ya existe ese nombre de usuario."});
 		else
 			return res.json({status: true, message: "Usuario registrado con éxito"});
 	});
