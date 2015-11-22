a565e077c6d4f31f7505681b0045762c4af8d4ed
Yuliana Apaza <yuliana.apaza94@gmail.com>
2015-11-22 17:18:45 -0500

    agregando cookies para login

--- app.js
+++ app.js
@@ -30,7 +30,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({keys: ['secretkey1', 'secretkey2', '...']}));
 app.use(express.static(path.join(__dirname, 'public')));
+
 //passport
 /*
 app.use(require('express-session')({
