b6610f73661e9062ad740388a761ea90ab75ae2e
roycer <roycercordova@gmail.com>
2015-11-22 14:27:18 -0500

    login modificando el redireccinamiento

--- public/angularJS/usuario.js
+++ public/angularJS/usuario.js
@@ -9,19 +9,16 @@ app.controller('myCtrlUsuario',  function($scope) {
 });
 
 app.controller('myCtrlUsuarioReg',  function($scope,$http) {
-	$scope.cuentaReg = {nombre:'a', apellidos:'a', usuario:'ss', correo:'a', clave:'s', claveVerificacion:'a'};
-	alert("estas en registrar");
+	$scope.cuentaReg = {nombre:'', apellidos:'', usuario:'', correo:'', clave:'', claveVerificacion:''};
+	
 	$scope.registrar = function(){
-		alert(JSON.stringify($scope.cuentaReg));
+		
 		$http.post('/fusuario/register', $scope.cuentaReg)
 		.success(function(data) {
-			
 				$scope.newPersona = {}; // Borramos los datos del formulario
-				$scope.personas = data;
-				alert(JSON.stringify($scope.personas));
-			})
-		.error(function(data) {
-			console.log('Error: ' + data);
+				$scope.respuestaServer = data;
+				alert(JSON.stringify($scope.respuestaServer));
+				
 		});
   };
 });
