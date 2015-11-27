4d5468d3fffaf168f6e6ab7e40ba3a3a6e99293c
Yuliana Apaza <yuliana.apaza94@gmail.com>
2015-11-27 11:02:18 -0500

    err

--- routes/fproyecto.js
+++ routes/fproyecto.js
@@ -16,33 +16,35 @@ router.post('/', function(req, res) {
 });
 
 router.post('/agregar', function(req, res) {
+	console.log(req.body);
 	var p = false, r = false;
 	var proy = new Proyecto({	nombre: req.body.nombre,
 								descripcion: req.body.descripcion,
 								fCulminacion: req.body.fechaCulminacion
 							});
-	proy.save(function(err){
+	proy.save(function(err) {
 		if (!err)
-			p = true;
-	});
-	if(p)
-	{
-		var i;
-		for(i = 0; i < req.body.usuarios.length; i++)
 		{
-			Usuario.findOne({username: req.body.roles[i].username}, function (err, usuario) {
-				console.log(usuario.notificaciones)
-				var nuevosRoles = usuario.roles;
-				nuevosRoles.push({rol: req.body.roles[i].rol, proyecto: proy._id});
-		
-				Usuario.update({username: usuario.username}, {roles: nuevosRoles}, function(err) {		
-					if(!err)
-						r = true;
+			var i;
+			for(i = 0; i < req.body.usuarios.length; i++)
+			{
+				Usuario.findOne({username: req.body.roles[i].username}, function (err2, usuario) {
+					if(!err2)
+					{
+						console.log(usuario.notificaciones)
+						var nuevosRoles = usuario.roles;
+						nuevosRoles.push({rol: req.body.roles[i].rol, proyecto: proy._id});
+	
+						Usuario.update({username: usuario.username}, {roles: nuevosRoles}, function(err3) {		
+							if(!err3)
+								return res.json({status: true, message: ""});
+						});
+					}
 				});
-			});
+			}
 		}
-	}
-	return res.json({status: p && r});
+	});
+	return res.json({status: false, message: ""})
 });
 
 module.exports = router;
