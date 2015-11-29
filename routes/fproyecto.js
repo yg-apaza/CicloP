04180fee441e2bf372732e843854e97e701d0909
yuliana <yuliana.apaza94@gmail.com>
2015-11-28 20:59:13 -0500

    c

--- routes/fproyecto.js
+++ routes/fproyecto.js
@@ -15,7 +15,7 @@ router.post('/', function(req, res) {
 });
 
 router.post('/modificar', function(req, res) {
-	if(req.body._id && req.body.nombre && req.body.descripcion && req.body.fechaCulminacion)
+	if(req.body._id && req.body.nombre && req.body.descripcion && req.body.fCulminacion)
 	{
 		Proyecto.update(
 			{_id: req.body._id},
