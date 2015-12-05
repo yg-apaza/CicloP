d4c3735c0bd066c4bd140b6e94fa0fdd014f38e5
Yuliana Apaza <yuliana.apaza94@gmail.com>
2015-12-05 14:56:34 -0500

    agregar lista

--- models/lista.js
+++ models/lista.js
@@ -4,14 +4,14 @@ var Schema = mongoose.Schema;
 var Lista = new Schema({
 	idProyecto: Schema.Types.ObjectId,
 	idModelo: Schema.Types.ObjectId,
-	nombre: String,
 	tipo: Number,
+	nombre: String,
 	etapa: Number,
 	fCreacion: {
 		type: Date,
 		default: Date.now()
 	},
-	fCulminacion: Date,
+	fCulminacion: String,
 	estado: Number, // 0: No publicado, 1: Publicado, 2: En proceso, 3: Aprobada, 4: Desaprobada
 	encargado: Schema.Types.ObjectId,
 	items: Array //[{seccion: int, item: int, estado: bool}]
