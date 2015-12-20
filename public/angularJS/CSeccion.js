b9b1260c3da190f3457b1214aaae2216cfc2c93a
roycer <roycercordova@gmail.com>
2015-12-19 21:37:09 -0500

    guardar y publicar

--- public/angularJS/CSeccion.js
+++ public/angularJS/CSeccion.js
@@ -4,7 +4,7 @@ app.controller('myCtrlSeccion',  function($scope,$http,$window) {
 
 	//$scope.items = [{nombre:'hoho'},{nombre:'naruto'}];
 	$http.post('/flista/getLista')
-	.success(function(data){
+		.success(function(data){
 			if(data.status){
 				$scope.secciones = data.secciones;
 			}
@@ -12,5 +12,24 @@ app.controller('myCtrlSeccion',  function($scope,$http,$window) {
 				alert("Problemas internos");
 	});
 
+	$scope.guardarSecciones = function (tipo){
+		
+		var urlGuardarSecciones = '/flista/guardarCambios';
 
+		if(tipo=1)
+			urlGuardarSecciones = '/flista/publicar';
+		
+
+		$http.post(urlGuardarSecciones,{secciones: $scope.secciones})
+			.success(function(data){
+				if(data.status){	
+					$scope.respuestaServidor = "Se guardaron correctamente";
+					$('#msjRespuesta').modal('show');
+				}
+				else{
+					$scope.respuestaServidor = "No se guardaron correctamente";
+					$('#msjRespuesta').modal('show');
+				}
+		});
+	}
 });
